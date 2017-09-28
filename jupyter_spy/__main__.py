dc4cc517fa7515c9942fb4d5795a85316ec1d413
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-09-28 18:41:24 +0200

    Add flag for filtering status messages

--- jupyter_spy/__main__.py
+++ jupyter_spy/__main__.py
@@ -6,6 +6,7 @@ import sys
 
 def build_spy_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--filter-status')
     parser.add_argument('connect_info', nargs=1)
     parser.add_argument('output', nargs='?')
     return parser
@@ -21,11 +22,12 @@ def run_spy(args=None):
     spy = Spy(info=options.connect_info[0])
 
     print('Starting logging, press CTRL+C to stop...', file=sys.stderr)
+    filter_function = is_not_status if options.filter_status else None
     if options.output:
         with open(options.output, 'w', encoding='utf8') as f:
-            spy.log_iopub(output=f, filter_function=is_not_status)
+            spy.log_iopub(output=f, filter_function=filter_function)
     else:
-        spy.log_iopub(filter_function=is_not_status)
+        spy.log_iopub(filter_function=filter_function)
 
 
 run_spy()
